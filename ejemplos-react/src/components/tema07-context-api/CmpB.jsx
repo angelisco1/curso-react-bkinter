733d255335b59cb1353d6711819688ad20028ed1
angelvil92 <angelvil92@gmail.com>
2026-05-27 14:05:19 +0200

    Día 3: useReducer

--- ejemplos-react/src/components/tema07-context-api/CmpB.jsx
+++ ejemplos-react/src/components/tema07-context-api/CmpB.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react"
 import { LangContext } from "./contexts/LangContext"
 import { Traductor } from "./utils/Traductor"
+import { UsuarioContext } from "./contexts/UsuarioContext"
 
 
 export const CmpB = () => {
   const langSeleccionado = useContext(LangContext)
+  const usuario = useContext(UsuarioContext)
 
   return (
     <>
       <h4>Componente B</h4>
       <p>Lang: {langSeleccionado}</p>
 
-      <p>{Traductor.traducir('bienvenido', langSeleccionado)} nombre</p>
+      <p>{Traductor.traducir('bienvenido', langSeleccionado)} {usuario.nombre}</p>
     </>
   )
 }
--- ejemplos-react/src/components/tema07-context-api/Tema07ContextAPI.jsx
+++ ejemplos-react/src/components/tema07-context-api/Tema07ContextAPI.jsx
@@ -1,14 +1,69 @@
-import { useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { CmpA } from "./CmpA"
 import { LangContext } from "./contexts/LangContext"
+import { UsuarioContext } from "./contexts/UsuarioContext"
+
+const reducer = (prevState, action) => {
+  console.log('PASA POR EL REDUCER')
+  switch (action.type) {
+    case 'INICIALIZAR_USUARIO':
+      return action.payload
+    case 'ELIMINAR_USUARIO':
+      return {nombre: '', email: ''}
+    case 'ACTUALIZAR_NOMBRE':
+      return {
+        ...prevState,
+        nombre: action.payload
+      }
+    default:
+      return prevState
+  }
+}
+
 
 export const Tema07ContextAPI = () => {
   const [lang, setLang] = useState('it')
+  const [usuario, dispatch] = useReducer(reducer, {nombre: '', email: ''})
+
+  useEffect(() => {
+    const usuario = localStorage.getItem('usuario')
+    if (usuario) {
+      // setUsuario(JSON.parse(usuario))
+      dispatch({type: 'INICIALIZAR_USUARIO', payload: JSON.parse(usuario)})
+    } else {
+      dispatch({type: 'INICIALIZAR_USUARIO', payload: {nombre: '', email: ''}})
+    }
+  }, [])
+
+  const changeNombre = (event) => {
+    event.preventDefault()
+
+    const nombre = event.target[0].value
+    localStorage.setItem('usuario', JSON.stringify({...usuario, nombre}))
+    dispatch({type: 'ACTUALIZAR_NOMBRE', payload: nombre})
+  }
+
+  const deleteUsuario = () => {
+    localStorage.removeItem('usuario')
+    dispatch({type: 'ELIMINAR_USUARIO'})
+  }
 
 
   return (
     <>
       <h2>Tema 07: context API y useContext</h2>
+
+    <form onSubmit={changeNombre}>
+      <div>
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" defaultValue={usuario.nombre} />
+      </div>
+      <div>
+        <button type="submit">Guardar</button>
+        <button type="button" onClick={deleteUsuario}>Eliminar</button>
+      </div>
+    </form>
+
 
       <div>
         <label htmlFor="lang">Selecciona el lenguaje de la app:</label>
@@ -21,7 +76,9 @@ export const Tema07ContextAPI = () => {
 
       {/* HOC: Higher Order Component */}
       <LangContext.Provider value={lang}>
-        <CmpA />
+        <UsuarioContext.Provider value={usuario}>
+            <CmpA />
+        </UsuarioContext.Provider>
       </LangContext.Provider>
 
     </>
